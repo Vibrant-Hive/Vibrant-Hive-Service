0502df21afc33df0057837a6f91557958d714cad
Mkaviyarasan <kaviyarasan1308@gmail.com>
2022-12-19 15:56:07 +0530

    initial code

---
+++ mentors/models.py
@@ -0,0 +1,20 @@
+# Create your models here.
+from django.db import models
+
+
+class User(models.Model):
+    active = models.BooleanField()
+    availability = models.CharField(max_length=100)
+    designation = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    experience = models.IntegerField(default=0)
+    full_name = models.CharField(max_length=100)
+    languages = models.CharField(max_length=100)
+    password = models.CharField(max_length=20)
+    rate = models.IntegerField(default=0)
+    resume = models.TextField()
+    role = models.CharField(max_length=200)
+    skills = models.CharField(max_length=200)
+
+
+
