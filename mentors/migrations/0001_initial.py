e51e098bd3599529631d5e7ea2994c9c1bb9e5cb
Mkaviyarasan <121018000+Mkaviyarasan@users.noreply.github.com>
2022-12-23 20:12:52 +0530

    Update 0001_initial.py

--- mentors/migrations/0001_initial.py
+++ mentors/migrations/0001_initial.py
@@ -31,7 +31,6 @@ class Migration(migrations.Migration):
                 ("languages", models.CharField(max_length=100)),
                 ("password", models.CharField(max_length=20)),
                 ("rate", models.IntegerField(default=0)),
-                ("resume", models.TextField()),
                 ("role", models.CharField(max_length=200)),
                 ("skills", models.CharField(max_length=200)),
             ],
