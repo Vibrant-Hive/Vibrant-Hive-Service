34311876985e1f6b98be2a1a9836a51c8de7500c
Mkaviyarasan <121018000+Mkaviyarasan@users.noreply.github.com>
2022-12-23 20:05:18 +0530

    Update views.py

--- mentors/views.py
+++ mentors/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from mentors.forms import UserForm
+
 
 # Create your views here.
+def create_account(request):
+    # if request.POST:
+    form = UserForm(json.loads(request.body.decode('utf-8')))
+    if form.is_valid():
+        save = form.save()
+        return HttpResponse("Success")
+    else:
+        errors = form.errors
+        print(errors)
+        return HttpResponse("Failed")
